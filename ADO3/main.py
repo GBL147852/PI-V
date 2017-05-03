2944c4f6b1b0c77aa4ab720eab16b7d15be54051
GBL147852 <gabriel.linss@hotmail.com>
2017-05-03 19:10:27 -0300

    Olá, Python!
    Bem vindo de volta ao projeto, Sir Python!

--- ADO3/main.py
+++ ADO3/main.py
@@ -2,8 +2,8 @@ import math
 
 #Constantes#
 
-#Desvio Padrao
-DPADRAO = 2
+#Desvio Padrao, ou sigma zero
+SIGMA0 = 2
 #Tau 1 dado no enunciado
 T1 = 3321.92
 #eta 0, ou seja, taxa de aprendizado
@@ -13,6 +13,14 @@ T2 = 1000
 #Raio de vizinhanca do neuronio vencedor
 R = 3
 
+#eta n, ou seja, taxa de aprendizado no momento n
+def eta(n):
+	return N0 * (math.pow( math.e , -n/T2 ))
+	
+#sigma n, ou seja, desvio padrao no momento n
+def sigma(n):
+	return SIGMA0 * (math.pow( math.e , n/T1 ))
+	
 #Retorna a distancia euclidiana 
 def distanciaEuclidiana(x, y, dimensions):
     distancia = 0
@@ -20,9 +28,18 @@ def distanciaEuclidiana(x, y, dimensions):
 		distancia += pow((x[i] - y[i]), 2)
     return math.sqrt(distancia)
 
+#Retorna o 	
+def h(nx,ny,n):
+	expoente = (-((distanciaEuclidiana(nx,ny,2))**2)) / (2 * (sigma(n)**2) )
+	return math.pow( math.e , expoente )
+
+#Funcao principal	
 def main():
+	i = 10000
 	A = [2,2]
-	B = [3,2]
-	print distanciaEuclidiana(A,B,2)
+	B = [90,30]
+	print h(A,B,i)
+	print eta(0)
+	print sigma(0)
 	
 main()
