af998997ebc33a707ef6c7a908a8c487e7c4b4a4
a <a@a.a>
2017-05-05 18:44:19 -0300

    Finalmente acentos!
    Refeitos alguns comentários e colocada a possibilidade de acento nos
    comentários.

--- ADO3/main.py
+++ ADO3/main.py
@@ -1,34 +1,38 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
 import math
 import random
 
 #Constantes#
 
-#Desvio Padrao, ou sigma zero
+#Desvio Padrão, ou σ(0)
 SIGMA0 = 2
-#Tau 1 dado no enunciado
+#T1 dado no enunciado
 T1 = 3321.92
-#eta 0, ou seja, taxa de aprendizado
+#η(0), ou seja, taxa de aprendizado
 N0 = 0.1
-#Tau 2 dado no enunciado
+#T(2) dado no enunciado
 T2 = 1000
-#Raio de vizinhanca do neuronio vencedor
+#Raio de vizinhança do neurônio vencedor
 R = 3
 
 
+#Retorna o quadrado perfeito mais próximo de um valor n
 def quadradoMaisProximo(n):
 	return int(math.ceil(math.sqrt(n*10)))
 
-#eta n, ou seja, taxa de aprendizado no momento n
+#η(n), ou seja, taxa de aprendizado no momento n
 def eta(n):
 	return N0 * (math.pow( math.e , -n/T2 ))
 	
 	
-#sigma n, ou seja, desvio padrao no momento n
+#σ(n), ou seja, desvio padrão no momento n
 def sigma(n):
 	return SIGMA0 * (math.pow( math.e , n/T1 ))
 	
 	
-#Retorna a distancia euclidiana entre x e y, dado d dimensoes
+#Retorna a distância euclidiana entre x e y, dado d dimensões
 def distanciaEuclidiana(x, y, d):
     distancia = 0
     for i in range(d):
@@ -45,16 +49,16 @@ def h(nx,ny,n):
 #Inicia randomicamente a matriz de pesos para cada atributo no mapa
 def iniciarPesos(atributos):
 	i = quadradoMaisProximo(atributos)
-	#Traduzindo: Pego o quadrado mais proximo (i) da quantidade de atributos (a)
+	#Traduzindo: Pego o quadrado mais próximo (i) da quantidade de atributos (a)
 	#			 e assim assumo uma matriz x[i][i][a]
 	return [[[random.random() for col in range(i)]for row in range(i)] for x in range(atributos)]
 	
 	
-#Funcao principal	
+#Função principal	
 def main():
 	i = 1
-	#Propositalmente, a distancia de 1, para que h(0) de 0.882497,
-	#assim como no grafico do enunciado (que eu corrigi pro Ilha)
+	#Propositalmente, a distância de 1, para que h(0) de 0.882497,
+	#assim como no gráfico do enunciado (que eu corrigi pro Ilha)
 	A = [2,2]
 	B = [3,2]
 	atributos = 4
@@ -63,8 +67,8 @@ def main():
 	for a in x:
 		print "Mapa de pesos do atributo " , i , "\n"
 		for linha in a:
-			#Printa cada vetor, arredondando em 4 casas decimais
-			print "%.4f " * len(linha) % tuple(linha)
+			#Printa cada vetor, arredondando em 5 casas decimais
+			print "%.5f " * len(linha) % tuple(linha)
 		print "\n\n"	
 		i+=1
 	print "- h(0) =\t" , h(A,B,0)
