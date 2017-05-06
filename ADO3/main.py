199c1acc8aadf796a11d9f0c5fa4aa37272138b5
Unknown <unphook@gmail.com>
2017-05-06 08:41:15 -0300

    oook acho q essa foi a parte 1

--- ADO3/main.py
+++ ADO3/main.py
@@ -3,11 +3,11 @@
 
 import math
 import random
-import stuff
+import stuff as s
 
-#Função principal
-def main():
-	som = stuff.Som(4)
+#só testes
+def testes():
+	som = s.Som(4,0,1)
 	entradas = [
 		[0.194444444,0.625,0.101694915,0.208333333],
 		[0.222222222,0.541666667,0.117644068,0.166666667],
@@ -25,5 +25,40 @@ def main():
 		valor = som.obterNeuronioVencedor(i)
 		print "entradas:", "%.5f " * len(i) % tuple(i)
 		print "neurônio:", valor[0], "x", valor[1], "\n"
+
+#parte 1 do projeto
+def parte1():
+	print "parte 1!"
+	som = s.Som(3,0,255)
+	entradas = [
+		[255,0	,0	],
+		[255,255,0	],
+		[0	,255,0	],
+		[0	,255,255],
+		[0	,0	,255],
+		[255,0	,255],
+	]
+	print "iterando!!"
+	n = 0
+	for i in xrange(som.iteracoes):
+		som.atualizarPesos(entradas[n])
+		n = (n+1)%len(entradas)
+	print "foi!!"
+	print "coisos encontrados pras entradas:\n"
+	for i in entradas:
+		valor = som.obterNeuronioVencedor(i)
+		print "entradas:", "%.5f " * len(i) % tuple(i)
+		print "neurônio:", valor[0], "x", valor[1], "\n"
+	#falta plotar isso!
+
+#parte 2 do projeto
+def parte2():
+	print "parte 2!"
+
+#Função principal
+def main():
+#	testes()
+	parte1()
+#	parte2()
 	raw_input()
 main()
--- ADO3/stuff.py
+++ ADO3/stuff.py
@@ -4,37 +4,14 @@
 import math
 import random
 
-#Constantes#
-
-#Desvio Padrão, ou σ(0)
-SIGMA0 = 2
-#T1 dado no enunciado
-T1 = 3321.92
-#η(0), ou seja, taxa de aprendizado
-N0 = 0.1
-#T(2) dado no enunciado
-T2 = 1000
-#Raio de vizinhança do neurônio vencedor
-R = 3
-
 
 #Retorna o quadrado perfeito mais próximo de um valor n
 def quadradoMaisProximo(n):
 	return int(math.ceil(math.sqrt(n*10)))
 	
 	
-#η(n), ou seja, taxa de aprendizado no momento n
-def eta(n):
-	return N0 * (math.pow( math.e , -n/T2 ))
-	
-	
-#σ(n), ou seja, desvio padrão no momento n
-def sigma(n):
-	return SIGMA0 * (math.pow( math.e , n/T1 ))
-	
-	
 #Retorna a distância euclidiana quadrática entre x e y, dado d dimensões
-def distanciaQuadratica(x, y, d):
+def distanciaQuadratica(x,y,d):
 	dist = 0
 	for i in range(d):
 		dist += (x[i]-y[i])**2
@@ -42,27 +19,50 @@ def distanciaQuadratica(x, y, d):
 	
 	
 #Retorna a distância euclidiana entre x e y, dado d dimensões
-def distancia(x, y, d):
+def distancia(x,y,d):
 	return math.sqrt(distanciaQuadratica(x,y,d))
 	
 	
-#Retorna o h_j,i_(n) dada a distância quadrática entre os pontos
-def h(d,n):
-	expoente = (-d) / (2 * (sigma(n)**2) )
-	return math.pow( math.e , expoente )
-	
-	
-#classes!!!!!
 class Som(object):
+	#número de iterações
+	n = 0
+	
+	#Desvio Padrão, ou σ(0)
+	SIGMA0 = 2
+	#T1 dado no enunciado
+	T1 = 3321.92
+	#η(0), ou seja, taxa de aprendizado
+	N0 = 0.1
+	#T(2) dado no enunciado
+	T2 = 1000
+	#Raio de vizinhança do neurônio vencedor
+	R = 3
+		
+		
+	#η(n), ou seja, taxa de aprendizado no momento n
+	def eta(self):
+		return self.N0 * (math.pow( math.e , -self.n/self.T2 ))
+		
+		
+	#σ(n), ou seja, desvio padrão no momento n
+	def sigma(self):
+		return self.SIGMA0 * (math.pow( math.e , self.n/self.T1 ))
+		
+		
+	#Retorna o h_j,i_(n) dada a distância quadrática entre os pontos
+	def h(self,d):
+		expoente = (-d) / (2 * (self.sigma()**2) )
+		return math.pow( math.e , expoente )
+		
+		
 	#inicializa o objeto com o tanto de atributos das entradas
-	def __init__(self,entradasLen):
-		self.n = 0
-		self.entradasLen = entradasLen
-		self.largura = quadradoMaisProximo(entradasLen)
+	def __init__(self,atributos,pesoMin,pesoMax):
+		self.atributos = atributos
+		self.largura = quadradoMaisProximo(self.atributos)
 		self.iteracoes = (self.largura**2)*500
 		#Traduzindo: Pego o quadrado mais próximo (i) da quantidade de atributos (a)
 		#			 e assim assumo uma matriz x[i][i][a]
-		self.pesos = [[[random.random() for x in range(self.entradasLen)]for i in range(self.largura)] for j in range(self.largura)]
+		self.pesos = [[[random.uniform(pesoMin,pesoMax) for x in range(self.atributos)]for i in range(self.largura)] for j in range(self.largura)]
 			
 		
 	#calcula o neurônio vencedor para as entradas dadas
@@ -72,7 +72,7 @@ class Som(object):
 		menorDist = -1
 		for i in range(self.largura):
 			for j in range(self.largura):
-				dist = distanciaQuadratica(entradas,self.pesos[i][j],self.entradasLen)
+				dist = distanciaQuadratica(entradas,self.pesos[i][j],self.atributos)
 				#se essa for a menor distância calculada (ou a primeira entrada),
 				#assume este neurônio como vencedor
 				#(fun fact, não precisa calcular a raiz da distância)
@@ -85,20 +85,27 @@ class Som(object):
 		
 	#atualiza os valores dos neurônios de acordo com uma nova entrada
 	def atualizarPesos(self,entradas):
+		#obtém o neurônio vencedor desse troço
 		nv = self.obterNeuronioVencedor(entradas)
-		for x in xrange(self.entradasLen):
+		#pré-calculando coisas! viva performance
+		eta = self.eta()
+		raioQuadrado = self.R**2
+		#arruma o peso dele
+		for x in xrange(self.atributos):
 			peso = self.pesos[nv[0]][nv[1]][x]
-			peso += eta(self.n)*(entradas[x]-peso)
+			peso += eta*(entradas[x]-peso)
 			self.pesos[nv[0]][nv[1]][x] = peso
-		irange = range(max(nv[0]-R,0),min(nv[0]+R+1,self.largura))
-		jrange = range(max(nv[1]-R,0),min(nv[1]+R+1,self.largura))
-		Rquadrado = R*R
+		#cata os vizinhos e arruma eles também
+		irange = range(max(nv[0]-self.R,0),min(nv[0]+self.R+1,self.largura))
+		jrange = range(max(nv[1]-self.R,0),min(nv[1]+self.R+1,self.largura))
 		for i in irange:
 			for j in jrange:
 				dist = (i-nv[0])**2 + (j-nv[1])**2
-				if dist <= Rquadrado:
-					for x in xrange(self.entradasLen):
+				if dist <= raioQuadrado:
+					etah = eta*self.h(dist)
+					for x in xrange(self.atributos):
 						peso = self.pesos[i][j][x]
-						peso += eta(self.n)*h(dist,self.n)*(entradas[x]-peso)
+						peso += etah*(entradas[x]-peso)
 						self.pesos[i][j][x] = peso
+		#:3c
 		self.n += 1
