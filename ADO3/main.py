cebbe6de4a33445cb17f3bf76fb012f35e11c0f4
Unknown <unphook@gmail.com>
2017-05-07 01:34:02 -0300

    organizadinha no código

--- ADO3/main.py
+++ ADO3/main.py
@@ -7,13 +7,18 @@ import som as s
 import reader
 import window
 
+#parte 1 dos resultados (cores)
 def parte1(iteracoesPorFrame=1,largura=0):
-	print "parte 1!"
+	print "parte 1!\n"
 	
-	#inicializa tudo
+	#inicializa uns valores
 	atributos = 3
 	if largura <= 0: largura = s.quadradoMaisProximo(atributos)
+	
+	#inicializa o som
 	som = s.Som(atributos=atributos,largura=largura,pesoMin=0,pesoMax=255)
+	
+	#entradas fixas. vermelho, amarelo, verde, turquesa, azul, rosa
 	entradas = [
 		[255,0	,0	],
 		[255,255,0	],
@@ -28,35 +33,47 @@ def parte1(iteracoesPorFrame=1,largura=0):
 	while True:
 		printEnd = 0
 		if window.loop():
+			
+			#realiza as iterações restantes
 			if som.n < som.iteracoes:
 				print "iteração", som.n, "/", som.iteracoes
 				for i in xrange(min(iteracoesPorFrame,som.iteracoes-som.n)):
+					
+					#aqui acontece a atualização dos pesos de fato
 					som.atualizarPesos(entradas[n])
 					n = (n+1)%len(entradas)
+					
 				if som.n >= som.iteracoes:
-					print "fim das iterações!"
+					print "fim das iterações!\n"
 					printEnd = 1
+					
 			window.drawMatrix(som.pesos)
 			window.frameEnd()
 		else:
 			printEnd = 2
 		if printEnd > 0:
-			print "coisos encontrados pras entradas:\n"
+			
+			#printa os resultados no fim das iterações
+			print "entradas e neurônios vencedores correspondentes:"
 			for i in entradas:
 				neu = som.obterNeuronioVencedor(i)
-				print "entradas:", "%.5f " * len(i) % tuple(i)
-				print "neurônio vencedor:", neu[0], "x", neu[1], "\n"
+				print i, "->", neu
 			if printEnd == 2: break
 
+#parte 2 dos resultados (dataset)
 def parte2(dataset,largura=0):
-	print "parte 2!"
+	print "parte 2!\n"
 	
-	#carrega o dataset e inicializa tudo
+	#carrega o dataset.
+	#o primeiro valor de cada linha do dataset é a classe,
+	#então ignoramos essa coluna sempre que possível
 	data = reader.load(dataset)
 	atributos = len(data[0])-1
 	if largura <= 0: largura = s.quadradoMaisProximo(atributos)
-	som = s.Som(atributos=atributos,largura=largura,pesoMin=0,pesoMax=1)
 	n = 0
+	
+	#inicializa o som
+	som = s.Som(atributos=atributos,largura=largura,pesoMin=0,pesoMax=1)
 	
 	#atribui as cores abaixo às classes
 	coresPossiveis = [
@@ -71,31 +88,42 @@ def parte2(dataset,largura=0):
 			coresPossiveis = coresPossiveis[1:]
 	
 	#itera!
-	print "iterando...", som.iteracoes
+	print "número de iterações:", som.iteracoes
+	print "iterando..."
 	while som.n < som.iteracoes:
+		
+		#atualização dos pesos aqui
 		som.atualizarPesos(data[n][1:])
 		n = (n+1)%len(data)
+		
+	print "fim das iterações!\n"
 	
-	#cria a matriz final
+	#cria a matriz de cores que será apresentada no loop da janela
 	matriz = [[[0 for x in xrange(3)] for i in xrange(som.largura)] for j in xrange(som.largura)]
 	for i in data:
 		neu = som.obterNeuronioVencedor(i[1:])
 		matriz[neu[0]][neu[1]] = cores[i[0]]
 		
-	#infos das cores
+	#printa infos sobre as classes e as cores
+	print "classes e cores correspondentes:"
 	for k,v in cores.iteritems():
-		print "classe", "%d" % k, "-> cor", v
+		print "%d" % k, "->", v
 	
 	#loop da janela
 	while window.loop():
 		window.drawMatrix(matriz)
 		window.frameEnd()
-			
+		
+#método principal
 def main():
+	print "\n"
 	window.start()
-#	parte1(largura=25,iteracoesPorFrame=50)
-#	parte2(dataset="breast")
-	parte2(dataset="iris")
+	
+#	parte1(iteracoesPorFrame=50)
+	parte2(dataset="breast")
+#	parte2(dataset="iris")
 #	parte2(dataset="wine")
+	
 	window.end()
+	print "\n"
 main()
